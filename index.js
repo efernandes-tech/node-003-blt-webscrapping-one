6cdfa3e07bd15fb7489573f2038343274fada2fd
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-08-21 17:41:14 -0300

    trabalhando com os dados

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ const options = {
     }
 }
 
+function processarDados(dados) {
+    // Salva no banco de dados.
+    console.log(JSON.stringify(dados))
+}
+
 rp(options)
     .then(($) => {
         console.log($)
+        const times = []
+        $('.tabela-body-linha').each((i, item) => {
+            const time = {
+                nome: $(item).find('.tabela-times-time-nome').text(),
+                posicao: parseInt($(item).find('.tabela-times-posicao').text())
+            }
+            if (time.nome !== "")
+                times.push(time)
+        })
+        processarDados(times)
     })
     .catch((err) => {
         console.log(err)
